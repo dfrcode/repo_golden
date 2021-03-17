6c8147d1660e82abb58cf4ca610efd0ad1fd19ea
Denis Frolov <denisfrolov.coder@gmail.com>
2021-03-17 18:51:56 +0300

    add script btn menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
 const copyright = document.querySelector('.box_copyright')
 copyright.innerHTML = `<p>&copy;Copyright ${new Date().getFullYear()} FreebiesXpress.com</p>`
+
+const btn = document.querySelector('.burger_btn')
+const menu = document.querySelector('.burger_menu')
+
+btn.addEventListener('click', () => {
+    if (menu.classList.length <= 1) {
+        menu.classList.add('active')
+    } else if (menu.classList.length > 1) {
+        menu.classList.remove('active')
+    }
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('active')
+})
